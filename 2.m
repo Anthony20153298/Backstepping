d00a59fbe1087b23754db39072bf25efc420bdec
Anthony20153298 <67616344+Anthony20153298@users.noreply.github.com>
2020-06-30 21:10:09 -0500

    Add files via upload

---
+++ 2.m
@@ -0,0 +1,8 @@
+﻿x1=linspace(-2,2,7);
+y1=linspace(-2,2,7);
+[x,y]=meshgrid(x1,y1);
+a=1;
+k=1;
+xdot2=(-2-a*cos(x))*(x+a*sin(x)+y)-x-k*(y+2*x+a*sin(x));
+xdot1=x+a*sin(x)+y;
+quiver(x1,y1,xdot1,xdot2)
---
+++ 7.m
@@ -0,0 +1,25 @@
+﻿function backstepping()
+to=0; % start time
+tf=10; % end time
+t=linspace(to,tf,100);
+xo(1)=2; % initial condition
+xo(2)=-3;
+[t,x] = ode45(@(t,x)nonlinear(t,x),t,xo);
+plot(t,x)
+xlabel('Time (s)')
+ylabel('Amplitude')
+grid
+function [xdot] = nonlinear(t,x)
+% nonlinear model to integrate with ODE45
+% parameters
+a=1; % parameter nominal value
+aU=-1+9*rand; % -1<a<8 Uncertainties in parameter
+k=1;
+% backstepping control
+u=(-2-a*cos(x(1)))*(x(1)+a*sin(x(1))+x(2))-x(1)-k*(x(2)+2*x(1)+a*sin(x(1)));
+%u=0; % no control action
+%xdot=[x(1)+a*sin(x(1))+x(2); % ideal system
+xdot=[x(1)+aU*sin(x(1))+x(2); % system with uncertainty aU
+u];
+endfunction
+endfunction
